c21bd263468422ef90381471b75972c4a128aca5
Harinder Labana <sunny.labana@gmail.com>
2022-04-24 17:10:14 -0400

    for and while loops for arrays and a reverse array using a for loop

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+const iceCreamFlavours = [
+  'chocolate',
+  'vanilla',
+  'cookies and cream',
+  'rocky road',
+  'strawberry',
+];
+
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+const lastItem = iceCreamFlavours[iceCreamFlavours.length - 1];
+console.log(lastItem);
+
+console.log(iceCreamFlavours.length);
--- x-marks-the-spot.js
+++ x-marks-the-spot.js
@@ -25,23 +25,3 @@ function finalPosition(moves) {
 }
 
 console.log(finalPosition(moves));
-
-// //picking the appropriate building for a voting station
-// function chooseStations(stations) {
-//   const goodStations = []; //array of suitable stations
-//   for (const station of stations) {
-//     //searching for station of stationS
-//     const capacity = station[1]; //capacity of the station, use index 1 of the arrays
-//     if (capacity >= 20) {
-//       //does capacity meet the requirement
-//       const type = station[2]; //create a variable to hold only the type of building that has met the capacity requriement.
-//       if (type === 'school' || type === 'community centre') {
-//         //only school and community centers are suitable buildings
-//         goodStations.push(station[0]); //push the suitable locations to a new array called goodStations [] the first entry of the suitable old array of arrays called station[0]
-//       }
-//     }
-//   }
-//   return goodStations; //return only the suitable locations
-// }
-
-// console.log(chooseStations(stations));
